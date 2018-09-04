f8e020228be6486b990c679adf413f201cfa9523
DELTATRE\Jeff.Huang <jeff.huang@deltatre.it>
2018-09-04 11:04:08 +0100

    create course page

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/course/CoursesPage';
 
 export default (
   // App component will always be loaded. then nest these other components as children. path /about will pass AboutPage component into App.js
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="courses" component={CoursesPage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
