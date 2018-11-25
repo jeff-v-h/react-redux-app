29409b3c0f6cb8a39d5b3669cac17e25dfc03041
jeffvh <jh0791@gmail.com>
2018-11-25 21:35:58 +0000

    add thunk and create load courses thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
     // secify all the middleware we want to use in the application
-    applyMiddleware(reduxImmutableStateInvariant())
+    applyMiddleware(thunk, reduxImmutableStateInvariant())
   );
 }
