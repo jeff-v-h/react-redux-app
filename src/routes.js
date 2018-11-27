4cbeac2227e4df06603199771792e510879464a4
jeffvh <jh0791@gmail.com>
2018-11-27 21:14:41 +0000

    create manage course page

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage';
 
 export default (
   // App component will always be loaded. then nest these other components as children. path /about will pass AboutPage component into App.js
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCoursePage} />
+    <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
