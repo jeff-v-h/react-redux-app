46f3695f01eee9fc1fd60aa754708c5cea65077a
jeffvh <jh0791@gmail.com>
2018-11-28 21:24:39 +0000

    create initialState and authorReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
   // sort hand property name
-  courses
+  courses,
+  authors
 });
 
 export default rootReducer;
