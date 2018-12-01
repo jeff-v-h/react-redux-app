fefb5b6b50997138a07addbf9ffab8ef733de042
jeffvh <jh0791@gmail.com>
2018-12-01 14:05:32 +0000

    create AJAX status actions and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
   // sort hand property name
   courses,
-  authors
+  authors,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
