d2f41329df6c7fe2eff6abfd0c558b755ed26086
jeffvh <jh0791@gmail.com>
2018-12-01 14:09:31 +0000

    call beginAjax in thunks

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -18,6 +19,7 @@ export function updateCourseSuccess(course) {
 // a thunk always expects a function that returns a dispatch
 export function loadCourses() {
   return function(dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
